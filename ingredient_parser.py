295a2571140f102e015b582c812bd3545db2dac8
bananasplit333 <134814180+bananasplit333@users.noreply.github.com>
2023-07-10 22:58:04 -0400

    moved files, optimized token parsing for documents

--- ingredient_parser.py
+++ ingredient_parser.py
@@ -77,7 +77,8 @@ system_message=f"""
     Each unordered list should only have one ingredient listed.\
     The customer query will be delimited with {delimiter} characters.\
     
-    Desired format: 
+    Desired format:
+    Name of Dish 
     <amount>: tablespoon, teaspoon should be abbreviated to tbsp and tsp. Convert every imperial measurement to metric (except volume).
     <ingredient name> is concise and does not contain extra details. 
     <preparation details> has a concise, brief description about the ingredient. For example, "onion finely chopped and diced" would equate to something like "onion, fine dice"
@@ -93,35 +94,6 @@ system_message=f"""
     Do not include brackets. Only output one measurement of each ingredient.
     """
     
-user_prompt = f"""
-    General tsos chicken served over a bowl of white rice
-    General Tso’s Chicken is a Chinese takeout go-to. Make it all in one dish in the comfort of your home for a sweet and spicy treat!
-    AD
-    Author: Natalya Drozhzhin
-    Course: Main Course
-    Cuisine: Asian
-    Keyword: general tsos chicken
-    Skill Level: Easy
-    Cost to Make: $10-$12
-    Calories: 386
-    Servings: 8 servings
-    Ingredients
-    2 lb chicken thighs, trimmed and cut into 1-inch pieces
-    1/2 cup corn starch
-    1/4 cup extra light olive oil, for frying, plus more as needed
-    2 tbsp minced ginger, from a 2-inch piece of ginger
-    3 cloves garlic, or 1 Tbsp grated or finely minced
-    1/2 tsp red pepper flakes, or added to taste
-    1 tsp sesame seeds, optional for garnish
-    General Tso's Sauce
-    1/2 cup cold water
-    5 tbsp low sodium soy sauce
-    3 tbsp rice vinegar, or more to taste
-    1 1/2 tbsp hoisin sauce
-    4 tbsp granulated sugar
-    1 1/2 tbsp cornstarch
-    """
-
 print("Welcome to the recipe parser!")
 for url in urls:
     user_prompt = get_ingredients(url)
@@ -130,6 +102,7 @@ for url in urls:
     {'role': 'user', 'content': f"{delimiter}{user_prompt}{delimiter}"}
     ]
     response = get_completion_from_messages(messages)
+    display(HTML(response))
     print(response)
 #response = get_completion_from_messages(messages)
 #print(response)
